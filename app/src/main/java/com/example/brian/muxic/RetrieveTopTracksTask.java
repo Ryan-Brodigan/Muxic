58699e099b1176ac46f6d0a37130d7217e099c32
All41n <allainescobar@outlook.com>
2018-11-22 16:18:40 +0000

    Code Cleanup

--- app/src/main/java/com/example/brian/muxic/RetrieveTopTracksTask.java
+++ app/src/main/java/com/example/brian/muxic/RetrieveTopTracksTask.java
@@ -1,25 +1,16 @@
 package com.example.brian.muxic;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.os.AsyncTask;
 import android.util.Log;
 import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
 import android.widget.Toast;
 import android.view.View;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
 
-import android.os.Handler;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 //Async Method that pulls the JSON from The Url.
@@ -32,11 +23,9 @@ public class RetrieveTopTracksTask extends AsyncTask<Void, Void, Void> {
 
     private static String lastFMURL = "http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=74f88c78b264c0f0bcb407833629961b&format=json";
     ArrayList<Track> libraryList;
-//    ArrayList<Track> withDuplicates;
     public RetrieveTopTracksTask(MainActivity context){
         this.context = context;
         this.libraryList = new ArrayList<>();
-//        this.withDuplicates = new ArrayList<>();
         this.listView = listView;
     }
 
@@ -59,16 +48,10 @@ public class RetrieveTopTracksTask extends AsyncTask<Void, Void, Void> {
         if(stringJson != null){
             try{
                 JSONObject jsonObj = new JSONObject(stringJson);
-
-                // Getting JSON Array node
                 JSONArray library = jsonObj.getJSONObject("tracks").getJSONArray("track");
-//                JSONArray getImageURL = jsonObj.getJSONObject("tracks").getJSONArray()
-//                String imageUrl;
-                //Track newTrack = new Track();
-                //ArrayList<Track> withDuplicates = new ArrayList<Track>();
+
                 for(int i = 0; i < library.length(); i++){
                     JSONObject lib = library.getJSONObject(i);
-//                    JSONArray getTrackDetail = lib.getJSONObject("tracks");
                     String trackName = lib.getString("name");
                     Integer playCount = lib.getInt("playcount");
 
@@ -83,25 +66,8 @@ public class RetrieveTopTracksTask extends AsyncTask<Void, Void, Void> {
 
                         Track newTrack = new Track(trackName,playCount,artistName,imageUrl);
 
-                        //Removing the duplicates using hashset
-                        //withDuplicates.add(newTrack);
-//                        withDuplicates.add(newTrack);
-//                        Set<Track> removeDuplicates = new HashSet<Track>(withDuplicates);
-
                         libraryList.add(newTrack);
                     }
-
-
-
-//                    JSONObject getArtist = lib.getJSONObject("artist");
-//                    String artistName = getArtist.getString("name");
-//                    HashMap<String,String> trackLibrary = new HashMap<>();
-//                    trackLibrary.put("name", trackName);
-//                    trackLibrary.put("playcount", playCount);
-//                    trackLibrary.put("name", artistName);
-
-//                     newTrack = new Track(trackName,playCount,artistName,imageUrl);
-
 
                 }
 
