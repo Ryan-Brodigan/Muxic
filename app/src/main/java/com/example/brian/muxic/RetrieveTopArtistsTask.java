fc92cf98b0c83265836119f7d1f3a9d640f0075b
AllainEs <allainescobar1@gmail.com>
2018-12-04 15:53:26 +0000

    MERGED UPDATES

--- app/src/main/java/com/example/brian/muxic/RetrieveTopArtistsTask.java
+++ app/src/main/java/com/example/brian/muxic/RetrieveTopArtistsTask.java
@@ -72,7 +72,9 @@ public class RetrieveTopArtistsTask extends AsyncTask<Void, Void, Void> {
                     images.add(smallImageUrl);
                     images.add(mediumImageUrl);
                     images.add(largeImageUrl);
+                    //Artist newArtists = new Artist(artistName,playCount,listeners,images);
                     Artist newArtists = new Artist(artistName,lastFMUrl,playCount,listeners,images);
+
                     artistList.add(newArtists);
                 }
 
