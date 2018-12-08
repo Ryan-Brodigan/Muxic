e4c9ca2aad176d2a6c8d6551a84e8b0902209618
Ryan Brodigan <brianbrodigan@hotmail.com>
2018-12-08 04:38:58 +0000

    Added navigation to the Favourites menu

--- app/src/main/java/com/example/brian/muxic/FavouritesMenuActivity.java
+++ app/src/main/java/com/example/brian/muxic/FavouritesMenuActivity.java
@@ -1,7 +1,10 @@
 package com.example.brian.muxic;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FavouritesMenuActivity extends AppCompatActivity {
 
@@ -9,5 +12,30 @@ public class FavouritesMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favourites_menu);
+
+        final Button viewFavouriteArtistsButton = findViewById(R.id.favourite_artists_button);
+        final Button viewFavouriteTracksButton = findViewById(R.id.favourite_tracks_button);
+
+        viewFavouriteArtistsButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                favouriteArtists();
+            }
+        });
+
+        viewFavouriteTracksButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                favouriteTracks();
+            }
+        });
+    }
+
+    public void favouriteArtists(){
+        Intent i = new Intent(FavouritesMenuActivity.this, DisplayFavouriteArtistsActivity.class);
+        startActivity(i);
+    }
+
+    public void favouriteTracks(){
+        Intent i = new Intent(FavouritesMenuActivity.this, DisplayFavouriteTracksActivity.class);
+        startActivity(i);
     }
 }
